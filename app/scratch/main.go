6f7000e39e8917d20c05ebbe75e859f200dad9f5
David Partouche <david.partouche@gmail.com>
2026-04-09 09:26:50 +0200

    scratch digital signature 2

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -22,15 +22,15 @@ func main() {
 }
 
 func run() error {
+	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
+	if err != nil {
+		return fmt.Errorf("unable to load private key for node: %w", err)
+	}
+
 	tx := Tx{
 		FromID: "Bill",
 		ToID:   "Aaron",
 		Value:  1000,
-	}
-
-	privateKey, err := crypto.LoadECDSA("zblock/accounts/kennedy.ecdsa")
-	if err != nil {
-		return fmt.Errorf("unable to load private key for node: %w", err)
 	}
 
 	data, err := json.Marshal(tx)
@@ -38,14 +38,58 @@ func run() error {
 		return fmt.Errorf("unable to marshal: %w", err)
 	}
 
-	v := crypto.Keccak256(data)
+	stamp := []byte(fmt.Sprintf("\x19Ardan Signed Message:\n:%d", len(data)))
+	v := crypto.Keccak256(stamp, data)
 
 	sig, err := crypto.Sign(v, privateKey)
 	if err != nil {
 		return fmt.Errorf("unable to sign: %w", err)
 	}
 
-	fmt.Println(hexutil.Encode(sig))
+	fmt.Println("SIG: " + hexutil.Encode(sig))
+
+	// =========================================================================
+	// OVER THE WIRE
+
+	publicKey, err := crypto.SigToPub(v, sig)
+	if err != nil {
+		return fmt.Errorf("unable to pub: %w", err)
+	}
+
+	fmt.Println("PUB: " + crypto.PubkeyToAddress(*publicKey).String())
+
+	// =========================================================================
+
+	tx2 := Tx{
+		FromID: "Bill",
+		ToID:   "Frank",
+		Value:  250,
+	}
+
+	data, err = json.Marshal(tx2)
+	if err != nil {
+		return fmt.Errorf("unable to marshal: %w", err)
+	}
+
+	stamp = []byte(fmt.Sprintf("\x19Ardan Signed Message:\n:%d", len(data)))
+	v2 := crypto.Keccak256(stamp, data)
+
+	sig2, err := crypto.Sign(v2, privateKey)
+	if err != nil {
+		return fmt.Errorf("unable to sign: %w", err)
+	}
+
+	fmt.Println("SIG: " + hexutil.Encode(sig2))
+
+	// =========================================================================
+	// OVER THE WIRE
+
+	publicKey, err = crypto.SigToPub(v2, sig2)
+	if err != nil {
+		return fmt.Errorf("unable to pub: %w", err)
+	}
+
+	fmt.Println("PUB: " + crypto.PubkeyToAddress(*publicKey).String())
 
 	return nil
 }
